c4af879d5e986bda292112603da38853a829147c
Pedro Ferreira <pedrodsf21@gmail.com>
2023-09-24 17:07:00 +0100

    Ex. 2.2 a)

---
+++ 02/code/kill_sig.cpp
@@ -0,0 +1,14 @@
+// #include <stdint.h>
+#include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/types.h>
+// #include <unistd.h>
+#include "process.h"
+
+int main(void) {
+  printf("Input PID: ");
+  int pid;
+  scanf("%d", &pid);
+  pkill((pid_t)pid, 2);
+  return 0;
+}
