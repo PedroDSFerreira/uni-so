66fcca03950dcb7c8232dd9da3a8e0897e90a7ce
Pedro Ferreira <pedrodsf21@gmail.com>
2023-09-28 19:12:37 +0100

    Ex. 3.2

---
+++ 03/code/counter/main.cpp
@@ -0,0 +1,48 @@
+#include <cassert>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "process.h"
+
+int main(int argc, char *argv[]) {
+
+  // creation:
+  int shmid = pshmget(IPC_PRIVATE, sizeof(int), 0600 | IPC_CREAT | IPC_EXCL);
+  // attach shm to pointer address:
+  int *pcounter = (int *)pshmat(shmid, NULL, 0);
+  *pcounter = 0;
+
+  if (pfork() == 0) {
+    // child
+    int max;
+    printf("Enter max [1:30]: ");
+    scanf("%d", &max);
+    assert(max > 1);
+    assert(max <= 30);
+
+    while (*pcounter < max) {
+      (*pcounter)++;
+      printf("\nChild: %d", *pcounter);
+      sleep(1);
+    }
+    exit(EXIT_SUCCESS);
+  } else {
+    // parent
+    pwait(NULL);
+
+    while (*pcounter > 1) {
+      (*pcounter)--;
+      printf("\nParent: %d", *pcounter);
+      sleep(1);
+    }
+  }
+
+  // detach shm from pointer address:
+  pshmdt(pcounter);
+  // destroy shm:
+  pshmctl(shmid, IPC_RMID, NULL);
+}
